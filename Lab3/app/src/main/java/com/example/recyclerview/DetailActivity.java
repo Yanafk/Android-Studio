9bf7912f3056c7906fd7db1889296d4bcab86d0a
Yanafk <yana33.99@mail.ru>
2020-11-17 12:33:00 +0700

    Завершенный проект

---
+++ Lab3/app/src/main/java/com/example/recyclerview/DetailActivity.java
@@ -0,0 +1,20 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.get("ARG_ITEM_ID").toString();
+
+        TextView textView = findViewById(R.id.details);
+        textView.setText(name);
+    }
+}
