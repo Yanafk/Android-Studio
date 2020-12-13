b047ee4f2109cde00214a14acb764f0ba00a2b24
Yanafk <yana33.99@mail.ru>
2020-12-13 18:58:36 +0700

    Добавление CommonGestures и завершение проекта

--- Lab5/EventHandling/app/src/main/java/com/example/eventhandling/MainActivity.java
+++ Lab5/EventHandling/app/src/main/java/com/example/eventhandling/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.eventhandling;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -12,21 +14,30 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.btn);
-        TextView textView = findViewById(R.id.tv_t);
-        TextView textView1 = findViewById(R.id.tv_text);
+        textView = findViewById(R.id.tv_t);
+        textView1 = findViewById(R.id.tv_text);
+        textView2 = findViewById(R.id.tv_me1);
+        textView3 = findViewById(R.id.tv_me2);
         textView.setVisibility(View.GONE);
         button.setVisibility(View.GONE);
         textView1.setVisibility(View.GONE);
+        textView2.setVisibility(View.GONE);
+        textView3.setVisibility(View.GONE);
 
     }
-
+    TextView textView;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    private GestureDetectorCompat gDetector;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity,menu);
@@ -37,26 +48,26 @@ public class MainActivity extends AppCompatActivity {
         // получим идентификатор выбранного пункта меню
         int id = item.getItemId();
         Button button = findViewById(R.id.btn);
-        final TextView textView = findViewById(R.id.tv_t);
-        TextView textView1 = findViewById(R.id.tv_text);
 
         // Операции для выбранного пункта меню
         switch (id) {
             case R.id.click_btn:
                 setTitle("ButtonClick");
-                textView.setVisibility(View.VISIBLE);
+                textView1.setVisibility(View.VISIBLE);
                 button.setVisibility(View.VISIBLE);
-                textView1.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
+                textView2.setVisibility(View.GONE);
+                textView3.setVisibility(View.GONE);
                 button.setOnClickListener(new Button.OnClickListener() {
                     public void onClick(View v) {
-                        textView.setText("Button clicked");
+                        textView1.setText("Button clicked");
                     }
                 }
                 );
                 button.setOnLongClickListener(
                         new Button.OnLongClickListener() {
                             public boolean onLongClick(View v) {
-                                textView.setText("Long button click");
+                                textView1.setText("Long button click");
                                 return true;
                             }
                         }
@@ -64,9 +75,11 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case R.id.m_event:
                 setTitle("MotionEvent");
-                textView.setVisibility(View.VISIBLE);
+                textView2.setVisibility(View.VISIBLE);
+                textView3.setVisibility(View.VISIBLE);
                 button.setVisibility(View.GONE);
-                textView1.setVisibility(View.VISIBLE);
+                textView1.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
                 ConstraintLayout myLayout = findViewById(R.id.main_activity);
                 myLayout.setOnTouchListener(new ConstraintLayout.OnTouchListener() {
                     public boolean onTouch(View v, MotionEvent m) {
@@ -81,6 +94,12 @@ public class MainActivity extends AppCompatActivity {
                 textView.setVisibility(View.VISIBLE);
                 button.setVisibility(View.GONE);
                 textView1.setVisibility(View.GONE);
+                textView2.setVisibility(View.GONE);
+                textView3.setVisibility(View.GONE);
+                ConstraintLayout myLayout1 = findViewById(R.id.main_activity);
+                myLayout1.setOnTouchListener(View::onTouchEvent);
+                this.gDetector = new GestureDetectorCompat(this,this);
+                gDetector.setOnDoubleTapListener(this);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -88,8 +107,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void handleTouch(MotionEvent m) {
-        TextView textView1 = findViewById(R.id.tv_t);
-        TextView textView2 = findViewById(R.id.tv_text);
         int pointerCount = m.getPointerCount();
         for (int i = 0; i < pointerCount; i++)
         {
@@ -122,9 +139,60 @@ public class MainActivity extends AppCompatActivity {
             String touchStatus = "Action: " + actionString + " Index: " +
                     actionIndex + " ID: " + id + " X: " + x + " Y: " + y;
             if (id == 0)
-                textView1.setText(touchStatus);
-            else
                 textView2.setText(touchStatus);
+            else
+                textView3.setText(touchStatus);
         }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        textView.setText ("onDown");
+        return true;
+    }
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        textView.setText("onFling");
+        return true;
+    }
+    @Override
+    public void onLongPress(MotionEvent event) {
+        textView.setText("onLongPress");
+    }
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+        textView.setText("onScroll");
+        return true;
+    }
+    @Override
+    public void onShowPress(MotionEvent event) {
+        textView.setText("onShowPress");
+    }
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        textView.setText("onSingleTapUp");
+        return true;
+    }
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        textView.setText("onDoubleTap");
+        return true;
+    }
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        textView.setText("onDoubleTapEvent");
+        return true;
+    }
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        textView.setText("onSingleTapConfirmed");
+        return true;
     }
 }
